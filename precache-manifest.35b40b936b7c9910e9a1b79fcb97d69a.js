cc76dca42b49ae4ec9e99180d809adf970bb634d
Ziv <ziv.israeli01@gmail.com>
2020-01-28 12:57:48 -0800

    Updates

---
+++ precache-manifest.35b40b936b7c9910e9a1b79fcb97d69a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0db77a42940e6e8c2f5ea42907c3158",
+    "url": "/mia-react/index.html"
+  },
+  {
+    "revision": "ee553dd7d8861b88bfa2",
+    "url": "/mia-react/static/css/main.8f839ef2.chunk.css"
+  },
+  {
+    "revision": "082d76abcb39c80abba0",
+    "url": "/mia-react/static/js/2.e558298b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/mia-react/static/js/2.e558298b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ee553dd7d8861b88bfa2",
+    "url": "/mia-react/static/js/main.b516f140.chunk.js"
+  },
+  {
+    "revision": "fc1c766b071e04afba1e",
+    "url": "/mia-react/static/js/runtime-main.a884c96e.js"
+  },
+  {
+    "revision": "26f2d9a50507d8a2783ad52450a10f8f",
+    "url": "/mia-react/static/media/footer-paw.26f2d9a5.png"
+  },
+  {
+    "revision": "0fe46424440ed773f3e18c1281b0bd0d",
+    "url": "/mia-react/static/media/heart-full.0fe46424.png"
+  },
+  {
+    "revision": "0fe46424440ed773f3e18c1281b0bd0d",
+    "url": "/mia-react/static/media/heart-likes.0fe46424.png"
+  },
+  {
+    "revision": "a49ca95cb4a0b46219069228b880e797",
+    "url": "/mia-react/static/media/heart-outline.a49ca95c.png"
+  },
+  {
+    "revision": "8cce3f069316f08735ea5ba93358b3a2",
+    "url": "/mia-react/static/media/img-frame.8cce3f06.png"
+  },
+  {
+    "revision": "ebc29d505d88d1f22ab054866efcabdf",
+    "url": "/mia-react/static/media/mia-about.ebc29d50.jpg"
+  }
+]);
