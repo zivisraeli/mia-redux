1949fe09f1b654072eda5898ed2ba7da47feee96
Ziv <ziv.israeli01@gmail.com>
2020-02-16 18:45:56 -0800

    Updates

--- precache-manifest.c3ae9286deca9930eb1c0092a8ed3543.js
+++ precache-manifest.c3ae9286deca9930eb1c0092a8ed3543.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "940f37f0c59bd132c1205fa15cde5a4d",
+    "revision": "232f73029da7410bf0eabc9dbfeecbbc",
     "url": "/mia-redux/index.html"
   },
   {
-    "revision": "3dd6a70cfb5cf31ed895",
-    "url": "/mia-redux/static/css/main.6c3f9174.chunk.css"
+    "revision": "63e255e3f5f265d5513e",
+    "url": "/mia-redux/static/css/main.f563358d.chunk.css"
   },
   {
     "revision": "1d47f1f704cda6263520",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/mia-redux/static/js/2.b5813cd0.chunk.js.LICENSE"
   },
   {
-    "revision": "3dd6a70cfb5cf31ed895",
+    "revision": "63e255e3f5f265d5513e",
     "url": "/mia-redux/static/js/main.a59cc43c.chunk.js"
   },
   {
