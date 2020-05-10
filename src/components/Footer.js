cb518a5c1dedc3912e8db01907994b6c8ae4e14b
Ziv <ziv.israeli01@gmail.com>
2020-05-10 12:53:31 -0700

    Style changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export const Footer = (props) => {
     <footer>
       <span>Disclaimer: This site is using cookies.</span>
       <span id="footer-divider">&nbsp;&nbsp;|&nbsp;&nbsp;</span>
-      <span id="footer-copyright">COPYRIGHTS &copy; 2019 Mia Israeli All Rights Reserved.&nbsp;
+      <span id="footer-copyright">COPYRIGHTS &copy; 2020 Mia Israeli All Rights Reserved.&nbsp;
         <img src={footerPawImg} alt="footer-paw" />
       </span>
     </footer>
